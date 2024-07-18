642d16d5f95dfa758bb8f1f2f0bbedb8806dd261
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-07-19 02:24:30 +0530

    loggs

--- frontend/src/components/URLForm.js
+++ frontend/src/components/URLForm.js
@@ -4,6 +4,7 @@ import URLList from './URLList'; // Import your URLList component
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // const API_BASE_URL = "http://localhost:8001"
+console.log('API_BASE_URL:', API_BASE_URL);
 
 function URLForm() {
   const [url, setUrl] = useState('');
